ac0933ee528cda40a12900b57edb362add512cbd
LH-10 <lalit1652h@gmail.com>
2025-12-31 02:36:20 +0530

    create router: create and setup solidjs router

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,7 +2,20 @@
 import { render } from 'solid-js/web'
 import './index.css'
 import App from './App.tsx'
+import { Route, Router } from '@solidjs/router'
+import Home from './pages/Home.tsx'
+import Movies from './pages/Movies.tsx'
+import MovieRate from './pages/MovieRate.tsx'
 
 const root = document.getElementById('root')
 
-render(() => <App />, root!)
+render(() =>{
+    return(
+    <Router root={App}>
+        <Route path={"/home"} component={Home}/>
+        <Route path={"/movie"} component={Movies}/>
+        <Route path={"/movier"} component={MovieRate}></Route>
+    </Router>
+    )
+}
+, root!)
