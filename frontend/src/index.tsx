ea35ed5b819aef8bf753f414b9d86911ce4a421f
LH-10 <lalit1652h@gmail.com>
2026-03-22 23:31:12 +0530

    add dev tools package

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -9,24 +9,23 @@ import MovieRate from "./pages/MovieRate.tsx";
 import AdminHome from "./adminpages/AdminHome.tsx";
 import AdminGames from "./pages/Admin/AdminGames.tsx";
 import AddAdminGames from "./pages/Admin/AdminAddGame.tsx";
-
+import "solid-devtools";
 const root = document.getElementById("root");
 
-
 render(() => {
-  return (<>
-    {/* <AdminHome /> */}
-    <Router >
-      
-        <Route path={"/"} component={AdminHome}/>
-        <Route path={"/home"} component={Home}/>
-        <Route path={"/movie"} component={Movies}/>
-        <Route path={"/games"} >
-          <Route path={"/"} component={AdminGames}/>
-          <Route path={"/addnew"} component={AddAdminGames}/>
+  return (
+    <>
+      {/* <AdminHome /> */}
+      <Router>
+        <Route path={"/"} component={AdminHome} />
+        <Route path={"/home"} component={Home} />
+        <Route path={"/movie"} component={Movies} />
+        <Route path={"/games"}>
+          <Route path={"/"} component={AdminGames} />
+          <Route path={"/addnew"} component={AddAdminGames} />
         </Route>
         <Route path={"/movier"} component={MovieRate}></Route>
-    </Router> 
-  </>
+      </Router>
+    </>
   );
 }, root!);
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,16 @@
-import { defineConfig } from 'vite'
-import solid from 'vite-plugin-solid'
-import tailwindcss from '@tailwindcss/vite'
-import solidPlugin from 'vite-plugin-solid'
+import { defineConfig } from "vite";
+import solid from "vite-plugin-solid";
+import tailwindcss from "@tailwindcss/vite";
+import solidPlugin from "vite-plugin-solid";
+import devtools from "solid-devtools/vite";
+
 export default defineConfig({
-  plugins: [  tailwindcss(),
-    solidPlugin()
-    ,solid()],
-})
+  plugins: [
+    devtools({
+      autoname: true,
+    }),
+    tailwindcss(),
+    solidPlugin(),
+    solid(),
+  ],
+});
